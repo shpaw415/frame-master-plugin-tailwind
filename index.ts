ac8ee141e802d8877ff2717e2ae80963a3f52411
Justin <shpaw415@gmail.com>
2025-10-22 21:54:30 -0400

    new way of doing

--- index.ts
+++ index.ts
@@ -1,9 +1,49 @@
 import { type FrameMasterPlugin } from "frame-master/plugin";
 import BunTailwindPlugin from "bun-plugin-tailwind";
 
-export default function createPlugin(): FrameMasterPlugin {
+export type TailwindPluginProps = {
+  inputFile: string;
+  outputFile: string;
+};
+
+declare global {
+  var __BUN_TAILWIND_PLUGIN_CHILD_PROCESS__:
+    | Bun.Subprocess<"ignore", "inherit", "inherit">
+    | undefined;
+}
+
+export default function createPlugin({
+  inputFile,
+  outputFile,
+}: TailwindPluginProps): FrameMasterPlugin {
+  const spawn = () => {
+    if (globalThis.__BUN_TAILWIND_PLUGIN_CHILD_PROCESS__) return;
+    __BUN_TAILWIND_PLUGIN_CHILD_PROCESS__ = Bun.spawn({
+      cmd: [
+        "bunx",
+        "@tailwindcss/cli",
+        "-i",
+        inputFile,
+        "-o",
+        outputFile,
+        "--watch",
+      ],
+      stdout: "inherit",
+      stderr: "inherit",
+    });
+  };
+
   return {
     name: "tailwind-plugin",
-    runtimePlugins: [BunTailwindPlugin],
+    serverStart: {
+      dev_main() {
+        const subProcess = globalThis.__BUN_TAILWIND_PLUGIN_CHILD_PROCESS__;
+        if (!subProcess || subProcess.exitCode !== null) {
+          spawn();
+        }
+      },
+    },
   };
 }
+
+export const TailwindPlugin = BunTailwindPlugin;
