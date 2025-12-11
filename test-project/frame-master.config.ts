22fad87d28c0e0391d7884f1f63045af6b2b2a3d
Justin Halle <shpaw415@gmail.com>
2025-12-10 19:12:45 -0500

    setup autoinject in build if setted to true

--- test-project/frame-master.config.ts
+++ test-project/frame-master.config.ts
@@ -1,7 +1,6 @@
 import type { FrameMasterConfig } from "frame-master/server/types";
 import TailwindPlugin from "..";
 import { isDev } from "frame-master/utils";
-import { cpSync } from "fs";
 
 const router = new Bun.FileSystemRouter({
   dir: "src",
@@ -33,6 +32,9 @@ export default {
     TailwindPlugin({
       inputFile: "static/tailwind.css",
       outputFile: "static/output.css",
+      options: {
+        autoInjectInBuild: false,
+      },
     }),
     {
       name: "html-entrypoints-plugin",
@@ -40,79 +42,6 @@ export default {
       build: {
         buildConfig: {
           entrypoints: Object.values(router.routes),
-          plugins: [
-            {
-              name: "remove-tailwind-link-plugin",
-              setup(build) {
-                const fmRemoveClass = "fm_remove";
-                const rewriter = new HTMLRewriter().on(
-                  `link.${fmRemoveClass}`,
-                  {
-                    element(element) {
-                      element.remove();
-                    },
-                  }
-                );
-                build.onLoad({ filter: /\.html$/ }, async (args) => {
-                  return {
-                    contents: rewriter.transform(
-                      (args.__chainedContents as string) ??
-                        (await Bun.file(args.path).text())
-                    ),
-                    loader: "html",
-                  };
-                });
-                build.onEnd(async (result) => {
-                  const rewriter = new HTMLRewriter().on("head", {
-                    element(element) {
-                      element.append(
-                        `<link href="/static/output.css" rel="stylesheet" />`,
-                        { html: true }
-                      );
-                    },
-                  });
-                  await Promise.all(
-                    result.outputs
-                      .filter((out) => out.path.endsWith(".html"))
-                      .map(async (out) => {
-                        await Bun.write(
-                          out.path,
-                          rewriter.transform(
-                            (await Bun.file(out.path).text()) as string
-                          )
-                        );
-                      })
-                  );
-
-                  cpSync("./static", "./.frame-master/build/static", {
-                    recursive: true,
-                  });
-
-                  const glob = Array.from(
-                    new Bun.Glob("**").scanSync({
-                      cwd: ".frame-master/build/static",
-                      onlyFiles: true,
-                      absolute: true,
-                    })
-                  ).filter((file) => !file.endsWith("tailwind.css"));
-
-                  result.outputs.push(
-                    ...glob.map(
-                      (file) =>
-                        ({
-                          ...Bun.file(file),
-                          hash: "",
-                          path: file,
-                          loader: "file",
-                          kind: "asset",
-                          sourcemap: null,
-                        } as Bun.BuildArtifact)
-                    )
-                  );
-                });
-              },
-            },
-          ],
         },
       },
     },
